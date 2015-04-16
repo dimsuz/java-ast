e49cb180bb6175fe518e6bc4911ba8e8fb6dfda4
Dmitry Suzdalev <dimsuz@gmail.com>
2015-04-16 12:05:54 +0200

    Parser now can parse comments and is starting to take shape

--- src/Main.hs
+++ src/Main.hs
@@ -1,23 +1,70 @@
 import System.IO
-import Text.Parsec
+import Text.Parsec hiding (token)
 import Text.Parsec.String
 
-line :: Parser String
-line = do
+data InputElement = Whitespace | C Comment | T Token | Unknown
+                    deriving (Eq,Ord,Show)
+data Comment = EndOfLineComment (Maybe String) | TraditionalComment (Maybe String)
+               deriving (Eq,Ord,Show)
+type Token = String
+
+inputElement :: Parser InputElement
+inputElement = whitespace <|> comment <|> token
+
+whitespace :: Parser InputElement
+whitespace = do
+  result <- many1 space <|> eol
+  return $ Whitespace
+
+comment :: Parser InputElement
+comment = simpleComment <|> multilineComment
+
+simpleComment :: Parser InputElement
+simpleComment = do
+  try (string "//")
   result <- many (noneOf "\n")
-  eol
-  return result
+  return $ C (EndOfLineComment (Just result))
 
-eol :: Parser Char
-eol = char '\n'
+multilineComment :: Parser InputElement
+multilineComment = do
+  try (string "/*")
+  result <- (manyTill anyChar (try $ string "*/"))
+  return $ C (TraditionalComment (Just result))
 
-file = do
-  result <- many line
-  eof
-  return result
+token :: Parser InputElement
+token = do
+  result <- many1 (noneOf "\n")
+  return $ T result
 
-parseJava input = parse file "unknown" input
+eol :: Parser String
+eol =  try (string "\n\r")
+    <|> try (string "\r\n")
+    <|> string "\n"
+    <|> string "\r"
+    <?> "end of line"
+
+elements :: Parser [InputElement]
+elements = many inputElement
+
+parseJava input = parse elements "unknown" input
+
+type ParseResult = Either ParseError [InputElement]
+
+filterResult :: (InputElement -> Bool) -> ParseResult -> ParseResult
+filterResult pred result = case result of
+  Right elements -> Right (filter pred elements)
+  Left error -> Left error
+
+prettyPrint :: ParseResult -> IO ()
+prettyPrint result = case result of
+  Right elements -> mapM_ (\e -> putStrLn $ show e) elements
+  Left error -> putStrLn $ show error
+
+isComment elem = case elem of
+  C _ -> True
+  otherwise -> False
 
 main = do
   contents <- readFile "/home/dima/projects/treto/TretoAndroid/app/src/main/java/ru/treto/tile/NewItemsManager.java"
-  putStrLn (show $ parseJava contents)
+  let parsed = parseJava contents
+  prettyPrint $ filterResult isComment parsed
